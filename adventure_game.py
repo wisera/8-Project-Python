f2ff6cf8b32b8815030c8b80800093f4ce99b8a5
wisera <luansurfer@hotmail.com>
2021-09-01 12:09:32 -0300

    project 7 and 8 w/ gui

--- adventure_game.py
+++ adventure_game.py
@@ -3,6 +3,7 @@
  # I want to reach different endings in my story, based on asnswers I give to Python
 
 # Whats the scenario? Im in war between 2 nations, and we have 2 sides: A and B. Only side A will win, and side B will lose! So I have to take the right decisions in order to win
+import PySimpleGUI as sg
 
 class AdventureGame:
     def __init__(self):
@@ -17,19 +18,37 @@ class AdventureGame:
 
 
     def Start(self):
-        answer1 = input(self.question1)
-        if answer1 == 'n' :
-            answer1B = input(self.question2)
-            if answer1B == 'sword':
-                print(self.finalStory1)
-            if answer1B == 'shield':
-                print(self.finalStory2)
-        if answer1 == 's':
-            answer1B = input(self.question3)
-            if answer1B == 'entry':
-                print(self.finalStory3)
-            if answer1B == 'clutch':
-                print(self.finalStory4)
-                
+        # Layout
+        layout = [
+            [sg.Output(size=(50,0), key='answers')],
+            [sg.Input(size=(25,0), key='choose')],
+            [sg.Button('Start'), sg.Button('Answer')]
+        ]
+        # create window
+        self.window = sg.Window('Adventure Game!', layout=layout)
+        while True:
+            # read data
+            self.ReadValues()
+            # do something w/ data
+            if self.event == 'Start':
+                print(self.question1)
+                self.ReadValues()
+                if self.values['choose'] == 'n' :
+                    print(self.question2)
+                    self.ReadValues()
+                    if self.values['choose'] == 'sword':
+                        print(self.finalStory1)
+                    if self.values['choose'] == 'shield':
+                        print(self.finalStory2)
+                if self.values['choose'] == 's':
+                    print(self.question3)
+                    self.ReadValues()
+                    if self.values['choose'] == 'entry':
+                        print(self.finalStory3)
+                    if self.values['choose'] == 'clutch':
+                        print(self.finalStory4)
+
+    def ReadValues(self):
+        self.event, self.values = self.window.Read()
 game = AdventureGame()
 game.Start()
